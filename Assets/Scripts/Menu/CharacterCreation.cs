73f69677113c2d73635cfc8201def3873be51798
jorashwil <35313725+jorashwil@users.noreply.github.com>
2018-01-13 23:49:45 -0500

    initial commit

---
+++ Assets/Scripts/Menu/CharacterCreation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreation : MonoBehaviour {
+
+	string className, playerName;
+	int exp = 0;
+	int level = 1;
+	int health;
+	int strength, constitution, dexterity, agility, intelligence, initiative, perception, morale, emotion = 4;
+	int statPoints = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Menu/Main.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	Button startGame;
+	Button changeOptions;
+	Button exit;
+	Canvas mainCanvas;
+	Canvas optionsCanvas;
+	string selectedMenuItem;
+
+	// Use this for initialization
+	void Start () {
+		startGame = GameObject.Find("PlayGame").GetComponent<Button>();
+		changeOptions = GameObject.Find("OptionsMenu").GetComponent<Button>();
+		exit = GameObject.Find("QuitGame").GetComponent<Button>();
+		mainCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (selectedMenuItem)
+		{
+			case "PlayGame":
+				SceneManager.LoadScene(1);
+				break;
+			case "OptionsMenu":
+				mainCanvas.enabled = false;
+				break;
+			case "QuitGame":
+				Application.Quit();
+				break;
+		}
+	}
+
+	void OnClick()
+	{
+		selectedMenuItem = EventSystem.current.currentSelectedGameObject.name;
+	}
+}
